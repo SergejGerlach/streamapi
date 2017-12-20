913444bed0e083a524b4b88c3af8be5a25e6fb70
Sergej Gerlach <sergej.gerlach@gmail.com>
2017-12-20 20:54:09 +0100

    test added

--- src/main/java/de/sergejgerlach/streamapi/tree/Mapper.java
+++ src/main/java/de/sergejgerlach/streamapi/tree/Mapper.java
@@ -1,10 +1,19 @@
 package de.sergejgerlach.streamapi.tree;
 
+import java.util.List;
+
 public class Mapper {
 
-    public static TreeNode map(TreeEntity entity) {
+    public static TreeNode mapEntity(TreeEntity entity) {
         if (entity == null) throw new IllegalArgumentException("entity is null");
 
+        return new TreeNode(entity.getId(), entity.getParentId(), entity.getName());
+    }
+
+    public static TreeNode mapEntities(List<TreeEntity> entities) {
+        if (entities == null || entities.isEmpty()) throw new IllegalArgumentException("entities is null or empty");
+
+        TreeEntity entity = entities.get(0);
         return new TreeNode(entity.getId(), entity.getParentId(), entity.getName());
     }
 }
--- src/test/java/de/sergejgerlach/streamapi/tree/MapperTest.java
+++ src/test/java/de/sergejgerlach/streamapi/tree/MapperTest.java
@@ -2,25 +2,75 @@ package de.sergejgerlach.streamapi.tree;
 
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.util.Lists.emptyList;
+import static org.assertj.core.api.Assertions.*;
+
 
 public class MapperTest {
 
     @Test
-    public void givenNull_when_map_throwsException() {
+    public void givenNull_when_mapEntity_throwsException() {
         // given / when / then
-        assertThatThrownBy(() -> Mapper.map(null)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Mapper.mapEntity(null)).isInstanceOf(IllegalArgumentException.class);
 
     }
 
     @Test
-    public void givenTreeEntity_when_map_returnsMappedTreeNode() {
+    public void givenTreeEntity_when_mapEntity_returnsMappedTreeNode() {
         // given
         TreeEntity entity = new TreeEntity("1.1", "1", "node-1.1");
 
         // when
-        TreeNode sut = Mapper.map(entity);
+        TreeNode sut = Mapper.mapEntity(entity);
+
+        // then
+        assertThat(sut).extracting("id", "parentId", "name", "children")
+                .contains("1.1", "1", "node-1.1", null);
+    }
+
+    @Test
+    public void givenNull_when_mapEntities_throwsException() {
+        // given / when / then
+        assertThatThrownBy(() -> Mapper.mapEntities(null)).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
+    public void givenEmpty_when_mapEntities_throwsException() {
+        // given / when / then
+        assertThatThrownBy(() -> Mapper.mapEntities(emptyList())).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
+    public void givenListOfOneTreeEntity_when_mapEntities_returnsMappedTreeNode() {
+        // given
+        List<TreeEntity> entities = new ArrayList<>();
+        entities.add(new TreeEntity("1.1", "1", "node-1.1"));
+
+        // when
+        TreeNode sut = Mapper.mapEntities(entities);
+
+        // then
+        assertThat(sut).extracting("id", "parentId", "name", "children")
+                .contains("1.1", "1", "node-1.1", null);
+    }
+
+    @Test
+    public void givenListOfManyTreeEntity_when_mapEntities_returnsMappedTreeNode() {
+        // given
+        List<TreeEntity> entities = new ArrayList<>();
+        entities.add(new TreeEntity("1.1", "1", "node-1.1"));
+        entities.add(new TreeEntity("1.2", "1", "node-1.1"));
+        entities.add(new TreeEntity("1.3", "1", "node-1.1"));
+        entities.add(new TreeEntity("1.4", "1", "node-1.1"));
+        entities.add(new TreeEntity("1.5", "1", "node-1.1"));
+
+        // when
+        TreeNode sut = Mapper.mapEntities(entities);
 
         // then
         assertThat(sut).extracting("id", "parentId", "name", "children")
